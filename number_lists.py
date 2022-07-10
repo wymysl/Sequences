5569a7eb21739b2b8c635467d85e5cd681fd5c2d
Piotr Wymyslowski <kuropatwinsky@gmail.com>
2022-07-10 08:46:09 +0200

    Solved puzzle
    My sister taught me about dictionaries in python, so I made this program.

--- number_lists.py
+++ number_lists.py
@@ -1,14 +1,11 @@
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-print(min(even))
-print(max(even))
-print(min(odd))
-print(max(odd))
+even.extend(odd)
+print(even)
+another_even = even
+print(another_even)
 
-print()
-print(len(even))
-print(len(odd))
-
-print()
-print("mississippi".count("issi"))
+even.sort(reverse=True)
+print(even)
+print(another_even)
